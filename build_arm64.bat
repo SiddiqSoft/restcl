e74ccf29796f1a53250b4ebd0894085b8ac7f0ba
Abdulkareem Siddiq <73565559+abdulkareem-siddiq@users.noreply.github.com>
2026-05-11 20:55:04 -0700

    fixed the build settings for windows

---
+++ build_arm64.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d c:\Users\maas\source\repos\siddiqsoft\restcl
+call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsamd64_arm64.bat"
+cmake --preset Windows-arm64-Debug
+cmake --build --preset Windows-arm64-Debug
+ctest --preset Windows-arm64-Debug
