8410bf550bd587b60f21b55b901f66413913d2f0
Abdulkareem Siddiq <73565559+abdulkareem-siddiq@users.noreply.github.com>
2026-05-14 12:08:49 -0700

    updated again

--- pipeline_detect_vs_path.bat
+++ pipeline_detect_vs_path.bat
@@ -1,6 +1,5 @@
 @echo off
 rem detect the location of Visual Studio vcvars and set the path
-setlocal
 
 rem Path to vswhere.exe (usually installed with VS)
 set "vswhere=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
@@ -19,5 +18,6 @@ rem vcvarsall.bat location for VS 2017+
 set "VCVARS_PATH=%VS_PATH%\VC\Auxiliary\Build\vcvarsall.bat"
 echo "Found Visual Studio at: `%VS_PATH%`"
 echo "Found vcvarsall.bat at: `%VCVARS_PATH%`"
+echo "Saving the %VCVARS_PATH% to environment variables"
 echo ##vso[task.setvariable variable=VCVARS_PATH]%VCVARS_PATH%
-endlocal
+echo "done."
